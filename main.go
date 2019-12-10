b9add29530c07db6a103b224b556e644620fed15
c1982 <aspsrc@gmail.com>
2019-12-10 03:30:40 +0300

    refactor: find interface methods

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ type Imain interface {
 	Do(ekle string) bool
 	It() string
 	Make(code int, text string)
+	VoidFunc()
 }
 
 type Imptest struct {
 	name string
 }
 
+func (i *Imptest) It() string {
+	return i.name
+}
+
 func main() {
 
+	a := Imptest{}
+	a.It()
 }
--- main_test.go
+++ main_test.go
@@ -5,12 +5,59 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
-	"io/ioutil"
 	"testing"
 )
 
 func TestParseInterface(t *testing.T) {
 
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, "./main.go", nil, parser.AllErrors)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ast.Inspect(node, func(n ast.Node) bool {
+		switch t := n.(type) {
+		case *ast.TypeSpec:
+			if t.Name.IsExported() {
+
+				switch x := t.Type.(type) {
+				case *ast.InterfaceType:
+					fmt.Println("Interface:", t.Name.Name)
+					printMedhods(x.Methods.List)
+				case *ast.StructType:
+					fmt.Println("Struct:", t.Name.Name)
+
+					//onj := node.Scope.Lookup(t.Name.Name)
+
+				}
+			}
+		}
+		return true
+	})
+}
+
+func printMedhods(list []*ast.Field) {
+	for _, method := range list {
+		switch z := method.Type.(type) {
+		case *ast.FuncType:
+			fmt.Println("\tMethod:", method.Names[0].Name)
+			printFunctionNames(z.Params.List)
+		}
+	}
+}
+
+func printFunctionNames(list []*ast.Field) {
+
+	for _, sig := range list {
+		for _, namesig := range sig.Names {
+			fmt.Println("\t\t", namesig.Name, sig.Type)
+		}
+	}
+
+}
+
+func Test2(t *testing.T) {
 	fset := token.NewFileSet()
 
 	f, err := parser.ParseFile(fset, "./main.go", nil, parser.AllErrors)
@@ -19,95 +66,8 @@ func TestParseInterface(t *testing.T) {
 		t.Error(err)
 	}
 
-	//pksg, err := parser.ParseDir(fset, "./")
-	//ast.Print(fset, f)
-
-	//pksg[""].Files
-
-	im := map[string]*ast.FuncType{}
-
-	ast.Inspect(f, func(n ast.Node) bool {
-
-		switch x := n.(type) {
-		case *ast.TypeSpec:
-
-			if x.Name.IsExported() {
-
-				switch d := x.Type.(type) {
-				case *ast.StructType:
-					//Type'ı buradan yakala.
-
-				case *ast.InterfaceType:
-
-					//fmt.Println(x.Name)
-					list := d.Methods
-
-					for i := 0; i < len(list.List); i++ {
-
-						k := list.List[i]
-
-						for z := 0; z < len(k.Names); z++ {
-							//m := k.Names[z]
-							//fmt.Println(m.Obj.Name)
-						}
-
-						switch ftt := k.Type.(type) {
-						case *ast.FuncType:
-
-							im[k.Names[0].Name] = ftt
-
-							if ftt.Params.NumFields() > 0 {
-								//fmt.Println("Aldığı parametreler")
-
-								for ll := 0; ll < len(ftt.Params.List); ll++ {
-									//ppp := ftt.Params.List[ll]
-									//fmt.Print(ppp.Type, ",")
-								}
-							}
-
-							if ftt.Results.NumFields() > 0 {
-								//fmt.Println("Geri dönüş tipi")
-
-								for oo := 0; oo < len(ftt.Results.List); oo++ {
-									//rrr := ftt.Results.List[oo]
-									//fmt.Print(rrr.Type, ",")
-								}
-							}
-
-						}
-
-						//fmt.Println("")
-					}
-				}
-			}
-		}
-
-		return true
-	})
-
-	for k, v := range im {
-
-		fmt.Println(k)
-
-		for i := 0; i < len(v.Params.List); i++ {
-			fmt.Println(v.Params.List[i].Names, v.Params.List[i].Type)
-		}
-
-		if v.Results.NumFields() > 0 {
-			for p := 0; p < len(v.Results.List); p++ {
-				fmt.Println(v.Results.List[p].Names)
-			}
-		}
+	for k, v := range f.Scope.Objects {
+		fmt.Println(k, v)
 	}
-
-	structName := "Imptest"
-	structDeclare := ""
-
-	//struct'ı oku
-	//interface methodlarını ekle.
-	//dosyaya yaz.
-
-	fileData := []byte(structDeclare)
-	ioutil.WriteFile(structName+".go", fileData, 666)
 
 }
